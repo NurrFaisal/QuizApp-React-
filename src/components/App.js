67e8c647007d0d872ea6c8dbe403149b1f86851d
NurFaisal <nurfaisal@nexkraft.com>
2022-01-08 17:08:26 +0600

    second commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,16 @@
+import "../styles/App.css";
+import Layout from "./Layout";
+import Login from "./pages/Login";
+// import Home from "./pages/Home";
+// import Signup from "./pages/Signup";
+
 function App() {
   return (
-    <div className="App">
-      <div>Hello World</div>
-    </div>
+    <Layout>
+      {/* <Home /> */}
+      {/* <Signup /> */}
+      <Login />
+    </Layout>
   );
 }
 
