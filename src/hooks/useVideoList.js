ac671a7fac24de1054a4b2c029dc700a40d67d6a
NurFaisal <nurfaisal@nexkraft.com>
2022-01-22 19:55:28 +0600

    part 12 done

---
+++ src/hooks/useVideoList.js
@@ -0,0 +1,58 @@
+import { useEffect } from "react";
+import {
+  get,
+  getDatabase,
+  orderByKey,
+  query,
+  ref,
+  startAt,
+  limitToFirst,
+} from "firebase/database";
+import { useState } from "react/cjs/react.development";
+
+export default function useVideoList(page) {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [videos, setVideos] = useState([]);
+  const [hasMore, setHasMore] = useState(true);
+
+  useEffect(() => {
+    async function fetchVideos() {
+      const db = getDatabase();
+      const videoRef = ref(db, "videos");
+      const videoQuery = query(
+        videoRef,
+        orderByKey(),
+        startAt("" + page),
+        limitToFirst(8)
+      );
+
+      try {
+        setError(false);
+        setLoading(true);
+        const snapshot = await get(videoQuery);
+
+        if (snapshot.exists()) {
+          setVideos((prevVideos) => {
+            return [...prevVideos, ...Object.values(snapshot.val())];
+          });
+        } else {
+          setHasMore(false);
+          setLoading(false);
+        }
+      } catch (err) {
+        console.log(err);
+        setLoading(false);
+        setError(true);
+      }
+    }
+    fetchVideos();
+  }, [page]);
+
+  return {
+    loading,
+    error,
+    videos,
+    hasMore,
+  };
+}
